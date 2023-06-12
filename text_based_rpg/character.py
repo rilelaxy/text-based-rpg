a95f1bf62cf5d4cd6e467a8a0d0f0abfc0a3d380
rilelaxy <113722425+rilelaxy@users.noreply.github.com>
2023-06-11 20:55:22 -0700

    adding character module and Character class

---
+++ text_based_rpg/character.py
@@ -0,0 +1,11 @@
+""" Our Character Class
+"""
+import random
+
+class Character:
+    # constructor
+    def __init__(self, name):
+        self.name = name
+
+
+    # methods
---
+++ text_based_rpg/main.py
@@ -0,0 +1,7 @@
+""" This is the main program for our game. """
+
+from character import Character
+
+if __name__ == "__main__":
+    hero = Character("phred")
+    print(hero.name)
